588033d51fe8ac9a06c9f21a49966a70554a8e79
Kai Hoffman <notkaiho@gmail.com>
2019-04-19 11:40:45 +0100

    updated tests: update quality on normal items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,12 +1,26 @@
 require 'gilded_rose'
+require 'item'
 describe GildedRose do
-
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
     end
+
+    it "decreases quality each day for generic items" do
+      items = [Item.new("foo", 4, 4)]
+      expect(items[0].quality).to eq(4)
+      expect { GildedRose.new(items).update_quality() }.to change {items[0].quality }.by(-1)
+    end
+
+    it "decreases quality of generic items at 2x speed once sell-by has passed" do
+      items = [Item.new(name="expired generic", sell_in=0, quality=20)]
+      expect { GildedRose.new(items).update_quality() }.to change {items[0].quality}.by(-2)
+    end
+
+    
+
   end
 
 end
