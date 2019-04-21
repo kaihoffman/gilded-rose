b016fa28c5dcfd2f492c8c948b29f576413babcc
Kai Hoffman <notkaiho@gmail.com>
2019-04-21 13:24:53 +0100

    Minor changes mandated by rubocop

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,9 +5,8 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-
       if item.name == "Sulfuras, Hand of Ragnaros"
         maintain_legendary_item(item)
       elsif item.name == "Aged Brie"
@@ -22,42 +21,42 @@ class GildedRose
 
   private
 
-    def maintain_legendary_item(item)
+  def maintain_legendary_item(item)
+      # do_nothing
+  end
 
-    end
+  def aged_brie_update(item)
+    change_quality(item, 1) unless item.quality == 50
+    lower_sell_in(item, 1)
+  end
 
-    def aged_brie_update(item)
-      change_quality(item, 1) unless item.quality == 50
-      lower_sell_in(item, 1)
+  def backstage_pass_update(item)
+    if item.sell_in < 11 && item.sell_in > 6 && item.quality < 50
+      change_quality(item, 2)
+    elsif item.sell_in < 6 && item.quality < 50
+      change_quality(item, 3)
+    else change_quality(item, 1) unless item.quality == 50
     end
+    if item.sell_in.zero?
+      item.quality = 0
+    end
+    lower_sell_in(item, 1)
+  end
 
-    def backstage_pass_update(item)
-      if item.sell_in < 11 && item.sell_in > 6 && item.quality < 50
-        change_quality(item, 2)
-      elsif item.sell_in < 6 && item.quality < 50
-        change_quality(item, 3)
-      else change_quality(item, 1) unless item.quality == 50
-      end
-      if item.sell_in == 0
-        item.quality = 0
-      end
-      lower_sell_in(item, 1)
+  def regular_item_update(item)
+    if item.sell_in < 1
+      change_quality(item, -2) unless item.quality < 1
+    else
+      change_quality(item, -1)
     end
+    lower_sell_in(item, 1)
+  end
 
-    def regular_item_update(item)
-      if item.sell_in < 1
-        change_quality(item, -2) unless item.quality < 1
-      else
-        change_quality(item, -1)
-      end
-      lower_sell_in(item, 1)
-    end
+  def lower_sell_in(item, number_of_days)
+    item.sell_in -= number_of_days
+  end
 
-    def lower_sell_in(item, number_of_days)
-      item.sell_in -= number_of_days
-    end
-
-    def change_quality(item, value)
-      item.quality = item.quality + value
-    end
+  def change_quality(item, value)
+    item.quality = item.quality + value
+  end
 end
