97a46efa6ff4656192bbfe1bebcebdfd8ffebe4b
Kai Hoffman <notkaiho@gmail.com>
2019-04-23 15:13:07 +0100

    edited out 'this item does not work yet' from texttest_fixture

--- texttest_fixture.rb
+++ texttest_fixture.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby -w
 
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require File.join(File.dirname(__FILE__), './lib/gilded_rose')
 
 puts "OMGHAI!"
 items = [
@@ -12,11 +12,10 @@ items = [
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-  # This Conjured item does not work properly yet
-  Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+  Item.new(name="Conjured Mana Cake", sell_in=3, quality=6)
 ]
 
-days = 2
+days = 50
 if ARGV.size > 0
   days = ARGV[0].to_i + 1
 end
